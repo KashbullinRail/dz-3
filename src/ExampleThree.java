cdc5b7d7fd7135efbf5b046add267441fb96d210
KashbullinRail <kashbullinrail@gmail.com>
2022-04-19 18:01:31 +0300

    Домашнее задание №3 по разделу "Переменные"

--- src/ExampleThree.java
+++ src/ExampleThree.java
@@ -8,17 +8,20 @@
 public class ExampleThree {
     public static void main(String[] args) {
 
+        System.out.println();
         // Задача 1
+        System.out.println("Задание 1");
         byte b = 122;
         short s = 24456;
         int i = 1345456575;
         long l = 332433244234L;
         float f = 2.3341f;
         double d = -1.526685681;
-        System.out.println("\n  b = " + b + "  s = " + s + "  i = " + i + "  l = " + l + "  f = " + f + "  d = " + d);
+        System.out.println("  b = " + b + "  s = " + s + "  i = " + i + "  l = " + l + "  f = " + f + "  d = " + d);
         System.out.println("");
 
         // Задача 2
+        System.out.println("Задание 2");
         float FirstBoxer = 78.2f;
         float SecondBoxer = 82.7f;
         float Sum = FirstBoxer + SecondBoxer;
@@ -28,6 +31,7 @@ public class ExampleThree {
         System.out.println("");
 
         // Задача 3
+        System.out.println("Задание 3");
         int WeightOneBanane = 80;
         float WeightOneMilk = 105 / 100f;
         int WeightOneIceCream = 100;
@@ -44,6 +48,7 @@ public class ExampleThree {
         System.out.println("");
 
         // Задача 4
+        System.out.println("Задание 4");
         int LoseWeight = 7000;
         int LoseWeightSlow = 250;
         int LoseWeightHigh = 500;
@@ -56,6 +61,7 @@ public class ExampleThree {
         System.out.println("");
 
         // Задача 5
+        System.out.println("Задание 5");
         int MashaSalary = 67760;
         int DenisSalary = 83690;
         int ChristinaSalary = 76230;
